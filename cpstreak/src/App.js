ec7ac6d33bac7debf25b4b34955f295726935404
Smitkhobragade <100017129+Smitkhobragade@users.noreply.github.com>
2023-10-27 22:22:37 +0530

    Initialising the project Added Navbar Added Footer

---
+++ cpstreak/src/App.js
@@ -0,0 +1,15 @@
+// import { Link } from "react-router-dom"
+
+export default function App() {
+  return (
+    <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    {/* <Link to='/'>Home</Link> &nbsp; &nbsp;
+    <Link to='/About'>About</Link> &nbsp; &nbsp;
+    <Link to='/Contact'>Contact</Link> &nbsp; &nbsp; */}
+    
+    </div>
+  )
+}
---
+++ cpstreak/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+
+import {useState} from 'react'
+import {AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <div className='flex justify-between items-center h-24 mx-auto px-4 text-white bg-black rounded-xl w-[98%] m-4'>
+      {/* <Image src={zetlogo} className='h-10 -ml-5'/> */}
+      <button className='bg-[#2075f0] rounded p-2'>CP Streak</button>
+      <ul className='hidden md:flex'>
+        <li className='p-4'><Link href="/">Home</Link></li>
+        <li className='p-4'><Link href="/About">About us</Link></li>
+        <li className='p-4'><Link href="/Contact">Contact us</Link></li>
+        <li className='p-4'>Log In</li>
+        <li className='p-2'><button className='bg-[#2075f0] rounded p-2'>Sign Up</button></li>
+      </ul>
+
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} className='fixed right-10'/> : <AiOutlineMenu size={20} className='fixed left-10'  />  }
+      </div>
+
+      <div className={(nav) ? 'fixed left-0 top-0 w-[60%] h-full border-r bg-[#110011] border-r-gray-900 ease-in-out duration-500 z-50 ' : 'fixed left-[-100%] z-50 '}>
+        <ul className='pt-12 uppercase p-4'>
+          <li className='p-4 border-b border-gray-600' onClick={() => handleNav()}><Link href="/">Home</Link></li>
+          <li className='p-4 border-b border-gray-600' onClick={() => handleNav()}><Link href="/About">About us</Link></li>
+          <li className='p-4 border-b border-gray-600' onClick={() => handleNav()}><Link href="/Contact">Contact us</Link></li>
+          <li className='p-4 border-b border-gray-600' onClick={() => handleNav()}>Log In</li>
+          <li className='p-2'><button className='bg-[#2075f0] rounded p-2' onClick={() => handleNav()}>Sign Up</button></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
